3f6d0fd87fee41ce1e8a7334b6e777e1d101c125
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-09 00:54:18 +0500

    Typography

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import TypographyMui from "./component/TypographyMui";
+
+const App = () => {
+  return (
+    <div>
+      <TypographyMui />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/TypographyMui.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const TypographyMui = () => {
+  return (
+    <div>
+      <Typography variant="h1">H1 </Typography>
+      <Typography variant="h2">H1 </Typography>
+      <Typography variant="h3">H1 </Typography>
+      <Typography variant="h4">H1 </Typography>
+      <Typography variant="h5">H1 </Typography>
+      <Typography variant="h6">H1 </Typography>
+      <Typography variant="subtitle1">H1 </Typography>
+      <Typography variant="subtitle2">H1 </Typography>
+      <Typography variant="body1">H1 </Typography>
+      <Typography variant="body2">H1 </Typography>
+    </div>
+  );
+};
+
+export default TypographyMui;
